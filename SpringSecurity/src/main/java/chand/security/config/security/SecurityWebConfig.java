4edcc799746e94d7f8b3eafd33d18cd0d6c6b9eb
chand <shaikchand8832@gmail.com>
2023-07-07 21:49:57 +0530

    add jdbc UserDetailsManager

--- SpringSecurity/src/main/java/chand/security/config/security/SecurityWebConfig.java
+++ SpringSecurity/src/main/java/chand/security/config/security/SecurityWebConfig.java
@@ -1,66 +1,86 @@
 package chand.security.config.security;
 
-import java.io.IOException;
+import javax.sql.DataSource;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityWebConfig {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.formLogin();
-		http.httpBasic();
-		
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		
-		http.httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint() {
 
-			@Override
-			public void commence(HttpServletRequest request, HttpServletResponse response,
-					AuthenticationException authException) throws IOException, ServletException {
-
-				response.sendError(401);
-			}
-		});
+//		http.httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint() {
+//
+//			@Override
+//			public void commence(HttpServletRequest request, HttpServletResponse response,
+//					AuthenticationException authException) throws IOException, ServletException {
+//
+//				response.sendError(401);
+//			}
+//		});
 //		http.authorizeHttpRequests().anyRequest().authenticated();
 		http.authorizeHttpRequests(request -> {
 			request.antMatchers("/notices", "/contact").permitAll()
 					.antMatchers("/myLoans", "/myBalance", "/myCards", "/myAccount").authenticated();
-		});
+		}).formLogin().and().httpBasic();
 //		UserDetails userDetails = User.withDefaultPasswordEncoder().username("chand").password("chand")
 //				.authorities("User").build();
-		http.userDetailsService(getInMemoryUserDetailsManager());
+
+		http.userDetailsService(users(dataSource));
 		return http.build();
 	}
 
-	@Bean
 	public InMemoryUserDetailsManager getInMemoryUserDetailsManager() {
 		InMemoryUserDetailsManager detailsManager = new InMemoryUserDetailsManager();
-		UserDetails userDetails = User.withUsername("chand").password(passwordEncoder().encode("chand")).username("chand").authorities("USER").build();
+		UserDetails userDetails = User.withUsername("chand").password(passwordEncoder().encode("chand"))
+				.username("chand").authorities("USER").build();
 		detailsManager.createUser(userDetails);
 		return detailsManager;
 	}
-	
+
+//	@Bean
+//	 UserDetailsManager users(DataSource dataSource) {
+//		JdbcUserDetailsManager detailsManager = new JdbcUserDetailsManager(dataSource);
+//		detailsManager
+//				.setUsersByUsernameQuery("select username,password, enabled from user_info where username = ?");
+//		detailsManager.setAuthoritiesByUsernameQuery("select u.username,role.role from user_info u inner join user_role role on role.fk_user_id=u.id where u.username = ?");
+//		return detailsManager;
+//	}
+
+	@Bean
+	UserDetailsManager users(DataSource dataSource) {
+
+		JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+		users.setUsersByUsernameQuery("select username,password, enabled from user_info where username = ?");
+		users.setAuthoritiesByUsernameQuery(
+				"select u.username,role.role from user_info u inner join user_role role on role.fk_user_id=u.id where u.username = ?");
+		return users;
+	}
+
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return NoOpPasswordEncoder.getInstance();
 	}
 
 }
